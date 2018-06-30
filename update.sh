e60eda1340a009e740e9abf0a4a8eb2f28f37570
Brian Hurst <hursey013@protonmail.com>
2018-06-30 17:23:38 -0400

    Initial commit

---
+++ update.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+
+SIZE=1920x1080 #size of desktop
+TERMS=wallpaper,nature #comma separated list
+FEATURED_ONLY=true #limit results to photos included in curated collections
+LOCAL=/tmp/wallpaper.jpg #where to store wallpaper locally
+REMOTE=https://source.unsplash.com/"$SIZE"/?"$TERMS"
+
+if [ "$FEATURED_ONLY" = true ] ; then
+  REMOTE="$REMOTE,featured"
+  echo $REMOTE
+fi
+
+wget -q --spider "$REMOTE" #first test that REMOTE is reachable
+
+if [ "$?" -eq 0 ]; then
+  wget -O "$LOCAL" "$REMOTE"
+
+  PROPS=$(xfconf-query -c xfce4-desktop -l | grep last-image)
+
+  for PROP in $PROPS; do
+    xfconf-query -c xfce4-desktop -p "$PROP" -s "$LOCAL"
+  done
+fi
+
